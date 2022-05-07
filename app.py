3f268ef1fa791190b64fdf9c94e68854504b4827
ed <edwardwisejr@gmail.com>
2022-05-07 12:16:13 -0500

    added readme, added redirect to app and added new html

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return redirect("/index", code=302)
 
 
 @app.route('/<page>')
